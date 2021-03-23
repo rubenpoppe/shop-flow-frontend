f626f5bd77870dd3c9b201c2d781d65ba2b59659
Ruben Poppe <ruben.poppe@hotmail.com>
2021-03-23 21:31:45 +0100

    fix app shell

--- src/App.jsx
+++ src/App.jsx
@@ -9,14 +9,14 @@ import Scanner from './components/Scanner';
 function App() {
 	return (
 		<Router>
-			<Switch>
-				<Route path="/products" exact></Route>
-				<Route path="/products/:id"></Route>
-				<Route path="/scan"></Route>
-				<Route path="/basket"></Route>
-			</Switch>
-
 			<ThemeProvider theme={theme}>
+				<Switch>
+					<Route path="/products" exact></Route>
+					<Route path="/products/:id"></Route>
+					<Route path="/scan"></Route>
+					<Route path="/basket"></Route>
+				</Switch>
+
 				<BottomNav />
 			</ThemeProvider>
 		</Router>
