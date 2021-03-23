e59474ab3d98891c853cd633c65dd47f32b3d3eb
Ruben Poppe <ruben.poppe@hotmail.com>
2021-03-23 22:37:59 +0100

    add collapse bottomnav on scan

--- src/components/BottomNav.jsx
+++ src/components/BottomNav.jsx
@@ -1,34 +1,42 @@
 import React from 'react';
-import { BottomNavigation, BottomNavigationAction } from '@material-ui/core';
+import {
+	BottomNavigation,
+	BottomNavigationAction,
+	Collapse,
+} from '@material-ui/core';
 import { ListAlt, ShoppingBasket } from '@material-ui/icons';
 import { BarcodeScan } from 'mdi-material-ui';
 import { Link, useLocation } from 'react-router-dom';
 
 function BottomNav() {
+	const location = useLocation();
+
 	return (
-		<BottomNavigation value={useLocation().pathname} showLabels>
-			<BottomNavigationAction
-				component={Link}
-				to="/products"
-				label="Producten"
-				value="/products"
-				icon={<ListAlt />}
-			/>
-			<BottomNavigationAction
-				component={Link}
-				to="/scan"
-				label="Scan"
-				value="/scan"
-				icon={<BarcodeScan />}
-			/>
-			<BottomNavigationAction
-				component={Link}
-				to="/basket"
-				label="Winkelmandje"
-				value="/basket"
-				icon={<ShoppingBasket />}
-			/>
-		</BottomNavigation>
+		<Collapse in={location.pathname !== '/scan'} timeout={500}>
+			<BottomNavigation value={location.pathname} showLabels>
+				<BottomNavigationAction
+					component={Link}
+					to="/products"
+					label="Producten"
+					value="/products"
+					icon={<ListAlt />}
+				/>
+				<BottomNavigationAction
+					component={Link}
+					to="/scan"
+					label="Scan"
+					value="/scan"
+					icon={<BarcodeScan />}
+				/>
+				<BottomNavigationAction
+					component={Link}
+					to="/basket"
+					label="Winkelmandje"
+					value="/basket"
+					icon={<ShoppingBasket />}
+				/>
+			</BottomNavigation>
+		</Collapse>
 	);
 }
 
